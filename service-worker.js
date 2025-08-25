243c616d1689f84ab436b180328ea9c6f3576b73
Sanchita844-CTL <sumayasanchita05456@gmail.com>
2025-08-25 13:40:26 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
 const cacheName = 'aquaculture-monitor-cache-v1';
 const assetsToCache = [
-  '/',
-  '/index.html',
-  '/homepage.css',
-  '/homepage.js',
-  '/daily.html',
-  '/daily.css',
-  '/daily.js',
-  '/signup.html',
-  '/signup.js',
-  '/monthly.html',
-  '/monthly.css',
-  '/monthly.js',
-  '/dashboard.html',
-  '/dashboard.css',
-  '/dashboard.js',
-  '/alarm.mp3'
+  './',
+  './index.html',
+  './homepage.css',
+  './homepage.js',
+  './daily.html',
+  './daily.css',
+  './daily.js',
+  './signup.html',
+  './signup.js',
+  './monthly.html',
+  './monthly.css',
+  './monthly.js',
+  './dashboard.html',
+  './dashboard.css',
+  './dashboard.js',
+  './alarm.mp3'
 ];
 
 // Install service worker and cache files
