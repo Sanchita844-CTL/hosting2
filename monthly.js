e72c056c7cc59041ac13c6832d1b29b079e6a2c0
Sanchita844-CTL <sumayasanchita05456@gmail.com>
2025-08-25 13:37:57 +0600

    Update monthly.js

--- monthly.js
+++ monthly.js
@@ -93,8 +93,9 @@ toggleBtn.addEventListener('click', () => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(reg => console.log('Service Worker Registered', reg))
       .catch(err => console.log('Service Worker registration failed', err));
   });
 }
+
