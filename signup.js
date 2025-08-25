21d3e509b7612e6c819bbe5a5cd582416a6d801f
Sanchita844-CTL <sumayasanchita05456@gmail.com>
2025-08-25 11:28:44 +0600

    Update signup.js

--- signup.js
+++ signup.js
@@ -57,3 +57,12 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   });
 });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker Registered', reg))
+      .catch(err => console.log('Service Worker registration failed', err));
+  });
+}
+
+
